41c105c8d8f52b64425966536ca41a735feb4ea3
Henrique Rosa Freire <henriquerf2@gmail.com>
2023-07-17 19:02:45 -0300

    Exercicio03
    Exercicio03 resolvido

--- 02 Python guia inicial/Exercicio03.py
+++ 02 Python guia inicial/Exercicio03.py
@@ -5,3 +5,16 @@ Leia a idade do usuário e classifique-o em:
 – Adulto – acima de 18 anos
 -Se o usuário digitar um número negativo, mostrar a mensagem que a idade é inválida
 """
+
+idade = int(input('idade: '))
+
+if idade >= 0 and idade <= 12:
+    print("idade crassificada como crianca")
+elif idade >= 13 and idade <= 17:
+    print("idade crassificada como adolescente")
+elif idade >= 18:
+    print("idade crassificada como adulto")
+else:
+    print("idade invalida")
+
+
