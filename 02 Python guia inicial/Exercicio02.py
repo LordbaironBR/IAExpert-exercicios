471a676fb6fdcb99ad65244c27257da124d23394
Henrique Rosa Freire <henriquerf2@gmail.com>
2023-07-17 18:53:50 -0300

    Exercicio02
    Exercicio02 resolvido

--- 02 Python guia inicial/Exercicio02.py
+++ 02 Python guia inicial/Exercicio02.py
@@ -7,3 +7,17 @@ Tendo o valor da distância, basta calcular a quantidade de litros de combustív
 fórmula: LITROS_USADOS = DISTANCIA / 12.
 O programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância percorrida e a quantidade de litros utilizada na viagem
 """
+
+consumo_km = float(input("Consumo por KM: "))
+tempo_viagem = float(input('tempo de viagem: '))
+velocidade_media = float(input('velocidade media: '))
+distancia_percorrida = tempo_viagem * velocidade_media
+consumo_total = distancia_percorrida / consumo_km
+
+print(f'O carro percorreu um total de {distancia_percorrida}KM\n'
+      f'Gastandob um total de {consumo_total}l de combustivel.')
+
+
+
+
+
