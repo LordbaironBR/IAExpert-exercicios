240ef93a3e0d3ede9f1f451b2ec53987d8c3e179
Henrique Rosa Freire <henriquerf2@gmail.com>
2023-07-17 17:52:17 -0300

    Mudanca de diretorio
    Exercicio 01 resolvido e adicionado.
    
    Removido arquivo com o chalenge da becrowd, pois nao e esse o destino desse arquivo. Criado arquivo "Exercicio02"

--- 02 Python guia inicial/Exercicio01.py
+++ 02 Python guia inicial/Exercicio01.py
@@ -1,9 +1,16 @@
-#Ler 5 notas e informar a média
+#Ler dois números inteiros, executar e mostrar o resultado das seguintes operações: adição, subtração, multiplicação e divisão
 
-soma_das_notas = 0
-quantidade_de_notas = int(input('quantidade_de_notas: '))
-for i in range(quantidade_de_notas):
-    nota = float(input('nota: '))
-    soma_das_notas += nota
-print(f'A média das notas é igual á:\n {soma_das_notas / quantidade_de_notas}')
-    
+n1 = int(input('Numero 1:'))
+n2 = int(input('Numero 2:'))
+operacao = str(input(f'Qual operacao deseja realizas?\n'
+                     f'Adicao; Subtracao; Multicacao e divisao:'))
+
+if operacao == 'Soma':
+    print(f'Soma de {n1} e {n2} resulta em: {n1+n2}')
+elif operacao == 'Subtracao':
+    print(f'Subtracao de {n1} e {n2} resulta em: {n1-n2}')
+elif operacao == 'Multiplicacao':
+    print(f'Multiplicacao entre {n1} e {n2} resulta em: {n1*n2}')
+elif operacao == 'Divisao':
+    print(f'Divisao de {n1} e {n2} resulta em {n1/n2}')
+
