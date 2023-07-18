c88db459a712ed1c71247ce6dc27066c9b474e3c
Henrique Rosa Freire <henriquerf2@gmail.com>
2023-07-17 21:56:33 -0300

    Update Exercicio07.py
    Exercicio07 resolvido

--- 02 Python guia inicial/Exercicio07.py
+++ 02 Python guia inicial/Exercicio07.py
@@ -2,3 +2,19 @@
 Crie uma estrutura de repetição para fazer a leitura de 5 números inteiros e os armazene dentro de uma lista.
 Após a leitura, crie outra estrutura de repetição para somar todos os valores digitados
 """
+import random
+
+"""
+lista1 = []
+for i in range(5):
+    lista1.append(random.randint(-10, 10))
+print(sum(lista1))
+"""
+
+lista2 = []
+soma = 0
+for i in range(5):
+    lista2.append(random.randint(-10, 10))
+    soma += lista2[i]
+print(soma)
+
