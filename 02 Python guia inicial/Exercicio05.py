91175f0c05d76207bcdee971772ceaed5d471e3d
Henrique Rosa Freire <henriquerf2@gmail.com>
2023-07-17 21:38:31 -0300

    Update Exercicio05.py
    Exercicio05 realizado

--- 02 Python guia inicial/Exercicio05.py
+++ 02 Python guia inicial/Exercicio05.py
@@ -1,3 +1,11 @@
 """
 Ler 5 notas e informar a média
 """
+quantidade_notas = int(input('quantidade_notas: '))
+soma = float(0)
+for i in range(quantidade_notas):
+    nota = int(input(f'Digite a nota {i+1}: '))
+    soma += nota
+print(f'A media das notas:\n'
+      f'{soma / i}')
+
